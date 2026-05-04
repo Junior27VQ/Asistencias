2596896ffaa9e4bcb096edd07de82b4473f4fc95
Junior27VQ <favq1999@outlook.com>
2026-05-04 12:14:54 -0500

    FAVQ: student controller tested

---
+++ src/main/java/com/krakedev/asistencias/controller/ControladorAsistencia.java
@@ -0,0 +1,12 @@
+package com.krakedev.asistencias.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/asistencia")
+public class ControladorAsistencia {
+	
+
+}
---
+++ src/main/java/com/krakedev/asistencias/controller/ControladorEstudiantes.java
@@ -0,0 +1,44 @@
+package com.krakedev.asistencias.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.krakedev.asistencias.entidades.Estudiante;
+import com.krakedev.asistencias.servicios.ServicioEstudiante;
+
+@RestController
+
+@RequestMapping("/estudiante")
+public class ControladorEstudiantes {
+	private final ServicioEstudiante servicioEstudiante=new ServicioEstudiante(); 
+	
+	@PostMapping
+	public void guardar(@RequestBody Estudiante estudiante) {
+		servicioEstudiante.agregar(estudiante);
+	}
+	@GetMapping("/{cedula}")
+	public Estudiante buscar(@PathVariable String cedula) {
+		return servicioEstudiante.buscarPorCedula(cedula);
+	}
+	@DeleteMapping("/{cedula}")
+	public void eliminar(@PathVariable String cedula) {
+		servicioEstudiante.eliminar(cedula);
+	}
+	@PutMapping("/{cedula}")
+	public void actualizar(@PathVariable String cedula, @RequestBody Estudiante estudiante) {
+		servicioEstudiante.actualizar(cedula, estudiante);
+	}
+	@GetMapping
+	public ArrayList<Estudiante> lista(){
+		return servicioEstudiante.listar();
+	}
+
+}
